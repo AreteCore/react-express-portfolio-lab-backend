2ed89605a1f51e314d1321c0304aea1704d83e52
Richard Clarke <freshnewrich@gmail.com>
2022-05-21 11:00:56 -0700

    files, a bit of server

---
+++ server.js
@@ -0,0 +1,7 @@
+//import dependencies
+const express = require("express")
+const cors = require("cors")
+
+//import JSON
+const projects = require("./projects.json")
+const about = require("./about.json")
