73bc0f063c0d84d87bcf8f94a1426ee85bd7d836
Richard Clarke <freshnewrich@gmail.com>
2022-05-21 11:02:05 -0700

    middleware

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ const cors = require("cors")
 //import JSON
 const projects = require("./projects.json")
 const about = require("./about.json")
+
+//app object so we can use express
+const app = express()
+
+//middleware
+app.use(cors())
